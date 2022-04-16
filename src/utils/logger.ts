c09a25d8b91ca789b50c6e9d739f2bfd4f493bc8
Jakub Bazgier <kuba@flyhigh.pro>
2022-04-16 13:04:22 +0200

    Update logger (WIP)

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,21 +1,77 @@
-import chalk from 'chalk';
+import chalk, { Chalk } from 'chalk';
 import isUnicodeSupported from 'is-unicode-supported';
 
-type LogTypes = {
-  [key: string]: string;
+type LogTypes =
+  | 'none'
+  | 'success'
+  | 'awaiting'
+  | 'complete'
+  | 'debug'
+  | 'error'
+  | 'info'
+  | 'pause'
+  | 'start'
+  | 'stop'
+  | 'warning'
+  | 'skip'
+  | 'pending';
+
+type TypesMap = {
+  [type in LogTypes]: {
+    symbol: string;
+    color: Chalk;
+    label: string;
+  };
 };
 
-const types: LogTypes = {
-  info: chalk.blue('info'),
-  success: chalk.green('success'),
-  warning: chalk.yellow('warning'),
-  error: chalk.red('error'),
-  debug: chalk.magenta('debug'),
+const types: TypesMap = {
+  none: { symbol: '', label: '', color: chalk.gray },
+  awaiting: { symbol: '…', label: 'awaiting', color: chalk.blue },
+  complete: { symbol: '☑', label: 'complete', color: chalk.green },
+  debug: { symbol: '◯', label: 'debug', color: chalk.magenta },
+  error: { symbol: '✖', label: 'error', color: chalk.red },
+  info: { symbol: 'ℹ', label: 'info', color: chalk.blue },
+  pause: { symbol: '■', label: 'pause', color: chalk.yellow },
+  pending: { symbol: '☐', label: 'pending', color: chalk.magenta },
+  start: { symbol: '▶', label: 'start', color: chalk.green },
+  stop: { symbol: '■', label: 'stop', color: chalk.red },
+  success: { symbol: '✔', label: 'success', color: chalk.green },
+  skip: { symbol: '≫', label: 'skipping', color: chalk.blue },
+  warning: { symbol: '⚠', label: 'warning', color: chalk.yellow },
 };
 
-export const log = (message: string, _type: keyof typeof types = 'info') => {
-  const type = types[_type] ?? 'info';
+// Return string of formatted message
+const logMessage = (message: string, logType: LogTypes): string => {
+  const finalLabel = [];
+  const desiredType = types[logType] ?? null;
+  if (!desiredType) {
+    throw new Error('Wrong log type provided');
+  }
 
-  console.log(`${type} ${message}`);
+  const { symbol, label, color } = desiredType;
+  if (isUnicodeSupported() && symbol) {
+    finalLabel.push(symbol);
+  }
+
+  if (label) {
+    finalLabel.push(label);
+  }
+
+  return `${color(finalLabel.join(' '))} ${message}`.trim();
 };
+
+// Log formatted message
+export const log = (message: string, type: LogTypes) => {
+  console.log(logMessage(message, type));
+};
+
+type Logger = Record<LogTypes, typeof logMessage>;
+
+export const logger = Object.keys(types).reduce(
+  (acc, type) => ({
+    ...acc,
+    [type]: (message: string) => log(message, type as LogTypes),
+  }),
+  {} as Logger
+);
 
