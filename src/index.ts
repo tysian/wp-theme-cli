11b622591d162be6afc35a0667788d2136c5d971
Jakub Bazgier <kuba@flyhigh.pro>
2022-04-17 18:20:30 +0200

    Replace yargs with Commander

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
 import 'trace';
 import 'clarify';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import inquirer from 'inquirer';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
 import { acfGenerator } from './modules/acf-generator';
-// import { log } from './utils/logger';
+import { Command } from 'commander';
+import chalk from 'chalk';
+import { logger } from './utils/logger';
 
 async function bootstrap() {
-  yargs(hideBin(process.argv))
-    .showHelpOnFail(true)
-    .command('gen', 'Generate ACF flexible field files', {}, () => {
-      acfGenerator();
-    })
-    .demandCommand()
-    .parse();
+  inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
+  const program = new Command();
+
+  program
+    .name('wp-theme-cli')
+    .description('Generate, update & remove files in our WordPress theme')
+    .version('0.1.0');
+
+  const generateAcceptedTypes = ['modules'].map((s) => chalk.green(s)).join(', ');
+  program
+    .command('generate')
+    .alias('g')
+    .description('Generate files')
+    .argument('<type>', `what to generate, accepting: ${generateAcceptedTypes}`)
+    .action((type) => {
+      if (type === 'modules') acfGenerator();
+      else {
+        logger.none(`Wrong type, accepting only: ${generateAcceptedTypes}`);
+      }
+    });
+
+  program.parse();
 }
 
 export default bootstrap;
