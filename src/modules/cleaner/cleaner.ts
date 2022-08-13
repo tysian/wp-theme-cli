825182e77e1694658ea4378614204e9f6214beec
Jakub Bazgier <kuba@flyhigh.pro>
2022-08-13 13:59:51 +0200

    Statistics class

--- src/modules/cleaner/cleaner.ts
+++ src/modules/cleaner/cleaner.ts
@@ -5,6 +5,7 @@ import { logger, updateLogger } from '../../utils/logger.js';
 import { batchFiles } from './operations/batchFiles.js';
 import { selectConfig } from './helpers/selectConfig.js';
 import { installDependencies } from '../../utils/installDependencies.js';
+import { Statistics, StatisticsCollection } from '../../utils/Statistics.js';
 
 export const cleaner = async (): Promise<void> => {
   logger.none('WordPress template cleaner!');
@@ -15,45 +16,36 @@ export const cleaner = async (): Promise<void> => {
 
     const finalConfig = await selectConfig();
 
-    const statistics = {
-      unchanged: 0,
-      modified: 0,
-      removed: 0,
-      errors: 0,
-    };
+    const stat = new Statistics({
+      unchanged: {
+        value: 0,
+        description: `Files unchanged`,
+      },
+      modified: {
+        value: 0,
+        description: `Files removed`,
+      },
+      removed: {
+        value: 0,
+        description: `Files modified`,
+      },
+      errors: {
+        value: 0,
+        description: `Errors`,
+        color: chalk.red,
+      },
+    });
 
-    const updateStats = (success, type = 'errors') => {
-      if (typeof success === 'object') {
-        Object.keys(success).forEach((k) => {
-          statistics[k] += success[k];
-        });
-      } else if (success) statistics[type] += 1;
-      else if (success === null) statistics.unchanged += 1;
-      else statistics.errors += 1;
-    };
-
-    const timeStart = performance.now();
+    stat.startTimer();
     for await (const fileElement of finalConfig) {
       const { file, operationType, options = {} } = fileElement;
 
       // TODO: Use Promise.all() here?
       const handler = await batchFiles(file, operationType, options);
-      updateStats(handler);
     }
 
-    const timeEnd = performance.now();
-    console.log(
-      [
-        chalk.bold('\nOperation finished.'),
-        `Files unchanged: ${chalk.blue(statistics.unchanged)}`,
-        `Files removed: ${chalk.blue(statistics.removed)}`,
-        `Files modified: ${chalk.blue(statistics.modified)}`,
-        `Errors: ${chalk.red(statistics.errors)}`,
-        `Time elapsed: ${chalk.blue(`${(timeEnd - timeStart).toFixed(3)}ms`)}`,
-      ]
-        .filter((m) => String(m).length)
-        .join('\n')
-    );
+    stat.stopTimer();
+    logger.none(stat.getFormattedStats());
 
     await installDependencies();
   } catch (error) {
--- src/utils/Statistics.ts
+++ src/utils/Statistics.ts
@@ -1,8 +1,62 @@
-type StatisticProp = {
+import chalk, { ChalkInstance } from 'chalk';
+import { performance } from 'perf_hooks';
+
+export type StatisticProp = {
   value: number;
   description: string;
+  color?: ChalkInstance;
 };
 
+export type StatisticsCollection = Record<string, StatisticProp>;
+
 export class Statistics {
-  constructor(private statistics: Record<string, StatisticProp>) {}
+  private startTime = 0;
+
+  private timeElapsed = 0;
+
+  constructor(private statistics: StatisticsCollection) {}
+
+  startTimer() {
+    this.startTime = performance.now();
+  }
+
+  stopTimer() {
+    if (this.startTime <= 0) {
+      throw new Error('You need to start your timer first');
+    }
+
+    this.timeElapsed = performance.now() - this.startTime;
+  }
+
+  getFormattedTimer() {
+    if (this.timeElapsed <= 0) {
+      throw new Error('You need to start and stop your timer properly.');
+    }
+    return `${this.timeElapsed.toFixed(3)}ms`;
+  }
+
+  incrementStat(type: keyof typeof this.statistics, amount = 1) {
+    this.statistics[type].value += amount;
+  }
+
+  getStat(type: keyof typeof this.statistics) {
+    return this.statistics[type];
+  }
+
+  getFormattedStats() {
+    const formattedStats = Object.values(this.statistics).map((props) => {
+      const finalText = [];
+      if (props.description.trim()) {
+        finalText.push(`${props.description}:`);
+      }
+      const color = props?.color ?? chalk.blue;
+      finalText.push(color(props.value));
+      return finalText.join(' ').trim();
+    });
+    if (this.timeElapsed) {
+      formattedStats.push(`Time elapsed: ${this.getFormattedTimer()}`);
+    }
+
+    return formattedStats.filter((s) => String(s).length).join('\n');
+  }
 }
