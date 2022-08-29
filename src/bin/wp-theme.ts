70630fbd1b0893cc252c92093c8863f30cffd145
Jakub Bazgier <kuba@tysian.pl>
2022-08-29 18:14:34 +0200

    Add Node required version

--- src/bin/wp-theme.ts
+++ src/bin/wp-theme.ts
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
-import { bootstrap } from '../bootstrap.js';
+import { NODE_MINIMUM_VERSION } from '../constants.js';
 
-bootstrap();
+const currentVersion = process.versions.node;
+const currentMajorVersion = parseInt(currentVersion.split('.')[0], 10);
+
+if (currentMajorVersion < NODE_MINIMUM_VERSION) {
+  console.error(`Your Node.js version (v${currentVersion}) is not supported.`);
+  console.error(`Please use Node.js v${NODE_MINIMUM_VERSION} or higher.`);
+  process.exit(1);
+}
+
+import('../bootstrap.js').then(({ bootstrap }) => bootstrap());
