c70b46a05096d5f7536124afc9209091a5e7b170
Jakub Bazgier <kuba@tysian.pl>
2022-09-01 18:02:20 +0200

    Change default config selection based on existence of default config

--- src/modules/acf-generator/helpers/selectConfig.ts
+++ src/modules/acf-generator/helpers/selectConfig.ts
@@ -7,12 +7,14 @@ import { DEFAULT_CONFIG_PATH } from '../acf-generator.const.js';
 import { createNewConfig } from './createNewConfig.js';
 
 export const selectConfig = async (): Promise<AcfGeneratorConfig> => {
+  const defaultConfigExists = await fileExists(DEFAULT_CONFIG_PATH).catch(() => false);
+
   const { configType } = await inquirer.prompt([
     {
       type: 'list',
       message: 'Select config type',
       name: 'configType',
-      default: 'default',
+      default: defaultConfigExists ? 'external-config-file' : 'create-new',
       choices: [
         {
           name: 'Create new config',
@@ -36,13 +38,12 @@ export const selectConfig = async (): Promise<AcfGeneratorConfig> => {
 
   if (configType === 'external-config-file') {
     // Ask for external config path
-    const externalConfigFilePath = await fileExists(DEFAULT_CONFIG_PATH).catch(() => '');
     const { externalConfigFile } = await inquirer.prompt([
       {
         type: 'file-tree-selection',
         message: 'Select external config file',
         name: 'externalConfigFile',
-        default: externalConfigFilePath || null,
+        default: defaultConfigExists || null,
         validate: (item: string) => path.extname(item) === '.json' || `You need json extension`,
       },
     ]);
