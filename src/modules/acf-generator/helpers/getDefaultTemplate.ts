fd6b64c37fdabe7e86e5516394163a7e8a023bb6
Jakub Bazgier <kuba@tysian.pl>
2022-09-03 14:33:20 +0200

    Move public files to module-related directory

--- src/modules/acf-generator/helpers/getDefaultTemplate.ts
+++ src/modules/acf-generator/helpers/getDefaultTemplate.ts
@@ -3,4 +3,6 @@ import { ROOT_DIR } from '../../../constants.js';
 import { AvailableFileType } from '../acf-generator.config.js';
 
 export const getDefaultTemplate = (fileType: AvailableFileType) =>
-  path.resolve(`${ROOT_DIR}/../public/templates/template.${fileType.toLowerCase()}.ejs`);
+  path.resolve(
+    `${ROOT_DIR}/../public/acf-generator/templates/template.${fileType.toLowerCase()}.ejs`
+  );
