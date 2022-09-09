46d91584430172748c7af5ec3b0294061b4af6c0
Jakub Bazgier <kuba@tysian.pl>
2022-09-09 20:09:22 +0200

    Give husky & lint-staged one more chance...

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+export default {
+  'src/**/*.ts': [
+    './node_modules/.bin/prettier --write',
+    './node_modules/.bin/eslint --cache --fix',
+    () => './node_modules/.bin/tsc -p tsconfig.json --noEmit',
+  ],
+  '*.{json,md}': ['./node_modules/.bin/prettier --write'],
+};
