4fbaee723fe1d6d8c7851a42b33872d734cab8fc
Jakub Bazgier <kuba@tysian.pl>
2022-09-10 11:33:47 +0200

    Refactor: Change if statement to switch

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -30,9 +30,13 @@ export const bootstrap = async () => {
     .description('Generate files')
     .argument('<type>', `what to generate, accepting: ${loggerListElements(['modules'])}`)
     .action((type) => {
-      if (type === 'modules') acfGenerator();
-      else {
-        logger.none(`Wrong type, accepting only: ${loggerListElements(['modules'])}`);
+      switch (type) {
+        case 'modules':
+          acfGenerator();
+          break;
+        default:
+          logger.none(`Wrong type, accepting only: ${loggerListElements(['modules'])}`);
+          break;
       }
     });
 
