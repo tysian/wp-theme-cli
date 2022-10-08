06925c9fe7db68b0f82877f8a05cc01b3cfae3fa
Jakub Bazgier <kuba@tysian.pl>
2022-10-08 12:26:03 +0200

    refactor: remove zod from project

--- src/modules/cleaner/cleaner.config.ts
+++ src/modules/cleaner/cleaner.config.ts
@@ -1,86 +1,52 @@
-import { z } from 'zod';
 import { OperationType } from './cleaner.const.js';
 
-// Base operation
-export const baseOperationSchema = z.object({
-  operationType: z.nativeEnum(OperationType),
-  groupKey: z.string().optional(),
-  description: z.string().optional(),
-  input: z.union([z.string(), z.string().array()]),
-  exclude: z.union([z.string(), z.string().array()]).optional(),
-});
-export type BaseOperation = z.infer<typeof baseOperationSchema>;
+export type BaseOperation = {
+  operationType: OperationType;
+  groupKey?: string;
+  description?: string;
+  input: string | string[];
+  exclude?: string | string[];
+};
 
 // Remove directory operation
-export const removeDirectoryOperationSchema = baseOperationSchema.and(
-  z.object({
-    operationType: z.literal(OperationType.REMOVE_DIRECTORY),
-  })
-);
-export type RemoveDirectoryOperation = z.infer<typeof removeDirectoryOperationSchema>;
+export type RemoveDirectoryOperation = BaseOperation & {
+  operationType: OperationType.REMOVE_DIRECTORY;
+};
 
-// Remove file operation
-export const removeFileOperationSchema = baseOperationSchema.and(
-  z.object({
-    operationType: z.literal(OperationType.REMOVE_FILE),
-  })
-);
-export type RemoveFileOperation = z.infer<typeof removeFileOperationSchema>;
+export type RemoveFileOperation = BaseOperation & {
+  operationType: OperationType.REMOVE_FILE;
+};
 
-// Remove from JSON operation
-export const removeFromJSONOperationSchema = baseOperationSchema.and(
-  z.object({
-    operationType: z.literal(OperationType.REMOVE_FROM_JSON),
-    propertyPaths: z.union([z.string(), z.string().array()]),
-  })
-);
+export type RemoveFromJSONOperation = BaseOperation & {
+  operationType: OperationType.REMOVE_FROM_JSON;
+  propertyPaths: string | string[];
+};
 
-export type RemoveFromJSONOperation = z.infer<typeof removeFromJSONOperationSchema>;
+export type RemoveACFLayoutOperation = BaseOperation & {
+  operationType: OperationType.REMOVE_ACF_LAYOUT;
+  layouts: string | string[];
+};
 
-// Remove ACF Layout operation
-export const removeACFLayoutOperationSchema = baseOperationSchema.and(
-  z.object({
-    operationType: z.literal(OperationType.REMOVE_ACF_LAYOUT),
-    layouts: z.union([z.string(), z.string().array()]),
-  })
-);
+export type RemoveFileLineOperation = BaseOperation & {
+  operationType: OperationType.REMOVE_FILE_LINE;
+  search: string | string[];
+};
 
-export type RemoveACFLayoutOperation = z.infer<typeof removeACFLayoutOperationSchema>;
+export type Operation =
+  | RemoveDirectoryOperation
+  | RemoveFileLineOperation
+  | RemoveFileOperation
+  | RemoveFromJSONOperation
+  | RemoveACFLayoutOperation;
 
-// Remove ACF Layout operation
-export const removeFileLineOperationSchema = baseOperationSchema.and(
-  z.object({
-    operationType: z.literal(OperationType.REMOVE_FILE_LINE),
-    search: z.union([z.string(), z.string().array()]),
-  })
-);
+export type OperationGroup = {
+  key: string;
+  name: string;
+  operations: Operation[];
+};
 
-export type RemoveFileLineOperation = z.infer<typeof removeFileLineOperationSchema>;
-
-// Operation
-export const OperationSchema = z.union([
-  removeDirectoryOperationSchema,
-  removeFileLineOperationSchema,
-  removeFileOperationSchema,
-  removeFromJSONOperationSchema,
-  removeACFLayoutOperationSchema,
-]);
-
-export type Operation = z.infer<typeof OperationSchema>;
-
-// Operation group
-export const OperationGroupSchema = z.object({
-  key: z.string(),
-  name: z.string(),
-  operations: OperationSchema.array(),
-});
-export type OperationGroup = z.infer<typeof OperationGroupSchema>;
-
-// Cleaner config
-export const CleanerConfigSchema = z.object({
-  name: z.string().optional(),
-  description: z.string().optional(),
-  groups: OperationGroupSchema.array(),
-});
-
-export type CleanerConfig = z.infer<typeof CleanerConfigSchema>;
+export type CleanerConfig = {
+  name?: string;
+  description?: string;
+  groups: OperationGroup[];
+};
