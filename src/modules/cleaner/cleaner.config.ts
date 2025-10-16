e4ba8f8b6ed1d91821ef39960e047d8c8bbf6abb
Jakub Bazgier <kuba@flyhigh.pro>
2025-10-16 22:11:30 +0200

    refactor: update cleaner config to new version with zod validation (wip)

--- src/modules/cleaner/cleaner.config.ts
+++ src/modules/cleaner/cleaner.config.ts
@@ -1,52 +1,98 @@
-import { OperationType } from './cleaner.const.js';
+import * as z from 'zod';
+// import { OperationType } from './cleaner.const.js';
 
-export type BaseOperation = {
-  operationType: OperationType;
-  groupKey?: string;
-  description?: string;
-  input: string | string[];
-  exclude?: string | string[];
-};
+export const OPERATION_TYPE = {
+  REMOVE_FILE: 'REMOVE_FILE',
+  REMOVE_FILE_LINE: 'REMOVE_FILE_LINE',
+  REMOVE_DIRECTORY: 'REMOVE_DIRECTORY',
+  REMOVE_FROM_JSON: 'REMOVE_FROM_JSON',
+  REMOVE_ACF_LAYOUT: 'REMOVE_ACF_LAYOUT',
+} as const;
 
-// Remove directory operation
-export type RemoveDirectoryOperation = BaseOperation & {
-  operationType: OperationType.REMOVE_DIRECTORY;
-};
+export type OperationType = (typeof OPERATION_TYPE)[keyof typeof OPERATION_TYPE];
 
-export type RemoveFileOperation = BaseOperation & {
-  operationType: OperationType.REMOVE_FILE;
-};
+const BaseOperationSchema = z.object({
+  /** Describe the operation */
+  description: z.string().optional(),
+  /** Relative file path or list of files, you can also use glob here */
+  input: z.union([z.string(), z.array(z.string())]),
+  /** Exclude files from `input` list, useful for excluding files from glob */
+  exclude: z.union([z.string(), z.array(z.string())]).optional(),
+});
 
-export type RemoveFromJSONOperation = BaseOperation & {
-  operationType: OperationType.REMOVE_FROM_JSON;
-  propertyPaths: string | string[];
-};
+const RemoveFileOperationSchema = z.object({
+  /** Type of operation */
+  type: z.literal(OPERATION_TYPE.REMOVE_FILE),
+  ...BaseOperationSchema.shape,
+});
 
-export type RemoveACFLayoutOperation = BaseOperation & {
-  operationType: OperationType.REMOVE_ACF_LAYOUT;
-  layouts: string | string[];
-};
+export type RemoveFileOperation = z.infer<typeof RemoveFileOperationSchema>;
 
-export type RemoveFileLineOperation = BaseOperation & {
-  operationType: OperationType.REMOVE_FILE_LINE;
-  search: string | string[];
-};
+const RemoveDirectoryOperationSchema = z.object({
+  /** Type of operation */
+  type: z.literal(OPERATION_TYPE.REMOVE_DIRECTORY),
+  ...BaseOperationSchema.shape,
+});
 
-export type Operation =
-  | RemoveDirectoryOperation
-  | RemoveFileLineOperation
-  | RemoveFileOperation
-  | RemoveFromJSONOperation
-  | RemoveACFLayoutOperation;
+export type RemoveDirectoryOperation = z.infer<typeof RemoveDirectoryOperationSchema>;
 
-export type OperationGroup = {
-  key: string;
-  name: string;
-  operations: Operation[];
-};
+const RemoveFromJSONOperationSchema = z.object({
+  /** Type of operation */
+  type: z.literal(OPERATION_TYPE.REMOVE_FROM_JSON),
+  ...BaseOperationSchema.shape,
+  /** Remove selected properties using lodash `unset` path, eg. `'a[0].b.c'` */
+  propertyPaths: z.union([z.string(), z.array(z.string())]),
+});
 
-export type CleanerConfig = {
-  name?: string;
-  description?: string;
-  groups: OperationGroup[];
-};
+export type RemoveFromJSONOperation = z.infer<typeof RemoveFromJSONOperationSchema>;
+
+const RemoveACFLayoutOperationSchema = z.object({
+  /** Type of operation */
+  type: z.literal(OPERATION_TYPE.REMOVE_ACF_LAYOUT),
+  ...BaseOperationSchema.shape,
+  /** Look for ACF Flexible field layouts and remove them from ACF Local JSON file */
+  layouts: z.union([z.string(), z.array(z.string())]),
+});
+
+export type RemoveACFLayoutOperation = z.infer<typeof RemoveACFLayoutOperationSchema>;
+
+const RemoveFileLineOperationSchema = z.object({
+  /** Type of operation */
+  type: z.literal(OPERATION_TYPE.REMOVE_FILE_LINE),
+  ...BaseOperationSchema.shape,
+  /** Search for specific text and remove line if text was found */
+  search: z.union([z.string(), z.array(z.string())]),
+});
+
+export type RemoveFileLineOperation = z.infer<typeof RemoveFileLineOperationSchema>;
+
+const operation = z.discriminatedUnion('type', [
+  RemoveFileOperationSchema,
+  RemoveFileLineOperationSchema,
+  RemoveDirectoryOperationSchema,
+  RemoveFromJSONOperationSchema,
+  RemoveACFLayoutOperationSchema,
+]);
+
+export type Operation = z.infer<typeof operation>;
+
+export const cleanerConfigSchema = z.object({
+  /** @deprecated There is no need to name cleaner config */
+  name: z.string().default(''),
+  /** @deprecated You don't have to use description, use comments instead */
+  description: z.string().default(''),
+  /** */
+  groups: z
+    .record(
+      z.string(),
+      z.object({
+        /** Group name */
+        name: z.string(),
+        /** List of operations */
+        operations: z.array(operation).default([]),
+      })
+    )
+    .default({}),
+});
+
+export type CleanerConfig = z.infer<typeof cleanerConfigSchema>;
